6d83c7402b21e7c51f5fcb7b176191bf9e3925d0
Patrick King <patrick@king-5.net>
2018-08-19 09:14:18 -0400

    add network profile change script

---
+++ SetNetworkProfile.ps1
@@ -0,0 +1,7 @@
+﻿#run get-netconnectionprofile to see what the adapters are
+Get-NetConnectionProfile
+
+#choose the one you want to change and
+#change the connection to the profile desired
+#uncomment line below and update the Interface Index to set the profile
+#Set-NetConnectionProfile -InterfaceIndex 20 -NetworkCategory Private
